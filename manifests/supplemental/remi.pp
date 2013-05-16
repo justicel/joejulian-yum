3634727dfdfb5ca78fbbd9166c24c1e7380d8c82
Justice London <jlondon@syrussystems.com>
2013-05-16 17:21:47 -0400

    Small fix to remi repo.

--- manifests/supplemental/remi.pp
+++ manifests/supplemental/remi.pp
@@ -12,7 +12,7 @@ class yum::supplemental::remi {
         'remi' :
           descr    =>
             'Les RPM de remi pour Enterpise Linux $releasever - $basearch',
-          baseurl  => 'http://rpms.famillecollet.com/el$releasever.$basearch/',
+          baseurl  => 'http://rpms.famillecollet.com/el$releasever/$basearch/',
           enabled  => 1,
           gpgcheck => 1,
           gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-remi',
